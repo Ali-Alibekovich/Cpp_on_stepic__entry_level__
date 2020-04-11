9670972756d1cab284e34c8df1108f7a6f2d99de
HusseinHalifaev <HusseinHalifaev@github.com>
2020-04-12 02:07:25 +0300

    Просто так

---
+++ Section_2/cmake-build-debug/CMakeFiles/Section_2.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/79312/Desktop/GitHub/C++_on_stepic__entry_level__/Section_2/1_task.cpp" "C:/Users/79312/Desktop/GitHub/C++_on_stepic__entry_level__/Section_2/cmake-build-debug/CMakeFiles/Section_2.dir/1_task.cpp.obj"
+  "C:/Users/79312/Desktop/GitHub/C++_on_stepic__entry_level__/Section_2/2_task.cpp" "C:/Users/79312/Desktop/GitHub/C++_on_stepic__entry_level__/Section_2/cmake-build-debug/CMakeFiles/Section_2.dir/2_task.cpp.obj"
+  "C:/Users/79312/Desktop/GitHub/C++_on_stepic__entry_level__/Section_2/3_task.cpp" "C:/Users/79312/Desktop/GitHub/C++_on_stepic__entry_level__/Section_2/cmake-build-debug/CMakeFiles/Section_2.dir/3_task.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Section_2/cmake-build-debug/CMakeFiles/Section_2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Section_2.dir/1_task.cpp.obj"
+  "CMakeFiles/Section_2.dir/2_task.cpp.obj"
+  "CMakeFiles/Section_2.dir/3_task.cpp.obj"
+  "Section_2.exe"
+  "Section_2.exe.manifest"
+  "Section_2.pdb"
+  "libSection_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Section_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
