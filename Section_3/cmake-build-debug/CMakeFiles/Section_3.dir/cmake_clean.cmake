a5a50c0d7cf6179d527749698fcccd571117c989
HusseinHalifaev <HusseinHalifaev@github.com>
2020-04-14 16:19:34 +0300

    Section 3 (processing)

---
+++ Section_3/cmake-build-debug/CMakeFiles/Section_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Section_3.dir/main.cpp.obj"
+  "Section_3.exe"
+  "Section_3.exe.manifest"
+  "Section_3.pdb"
+  "libSection_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Section_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
