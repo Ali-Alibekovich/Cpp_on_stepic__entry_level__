80c6fe274ad2787b7496a3cf281dd7757a9dac50
HusseinHalifaev <HusseinHalifaev@github.com>
2020-04-16 00:39:57 +0300

    Processing

---
+++ Section_4/cmake-build-debug/CMakeFiles/Section_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Section_4.dir/main.cpp.obj"
+  "Section_4.exe"
+  "Section_4.exe.manifest"
+  "Section_4.pdb"
+  "libSection_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Section_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
