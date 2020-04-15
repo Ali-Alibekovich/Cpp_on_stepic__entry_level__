65cdb738f84b70fa5e8c5b9b08cbdd757dca8cb2
HusseinHalifaev <HusseinHalifaev@github.com>
2020-04-16 00:33:39 +0300

    added 6_task.cpp

---
+++ Section_3/6_task.cpp
@@ -0,0 +1,26 @@
+#include <algorithm> // std::swap
+#include <cstddef>   // size_t
+#include <cstring>   // strlen, strcpy
+
+struct String {
+    String(const char *str = "");
+    String(size_t n, char c);
+    String(const String &other);
+    ~String();
+
+    /* Реализуйте оператор присваивания */
+    String &operator=(const String &other){
+        if(this!=&other) {
+            delete[] str;
+            str=new char[other.size+1];
+            strcpy(str, other.str);
+            size = other.size;
+        }
+        return *this;
+    }
+
+    void append(const String &other);
+
+    size_t size;
+    char *str;
+};
